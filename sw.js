37d19d4d785c6da3d6930bb80a8e90bf6262b2d7
btocco3931 <briantocco@gmail.com>
2025-09-28 22:59:35 -0400

    Add offline support with service worker

---
+++ sw.js
@@ -0,0 +1,20 @@
+// sw.js - Service Worker for offline cache
+const CACHE_NAME = "toldviewer-v1";
+const ASSETS = [
+  "./",
+  "./index.html"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => {
+      return resp || fetch(e.request);
+    })
+  );
+});
